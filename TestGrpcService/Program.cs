cd32c91f5d3ba703d2da3360cb2012ecda896529
Keith Becker <keith.becker@recro.com>
2022-07-11 14:31:07 -0600

    Test

--- TestGrpcService/Program.cs
+++ TestGrpcService/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 app.UseGrpcWeb();
+app.UseRouting();
 // Configure the HTTP request pipeline.
 //app.MapGrpcService<GreeterService>();
 app.UseEndpoints(endpoints =>
